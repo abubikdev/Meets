e86440dbe569a25555c63e000721e8dcf9834458
abubikdev <a.bubik@icloud.com>
2026-09-15 13:15:33 +0200

    Initial Commit

---
+++ Meets/MeetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeetsApp.swift
+//  Meets
+//
+//  Created by Adam Bubík on 15.09.2026.
+//
+
+import SwiftUI
+
+@main
+struct MeetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

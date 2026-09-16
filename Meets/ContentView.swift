39e2be41dc7deb3b81080cdcd01cb76c9ae6a1d6
abubikdev <a.bubik@icloud.com>
2026-09-16 16:32:38 +0200

    Started working on the new tab bar

--- Meets/ContentView.swift
+++ Meets/ContentView.swift
@@ -1,32 +1,14 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var selectedTab: AppTab = .history
     @State private var isCreateSheetOpen: Bool = false
-    @State private var isShowingSearch: Bool = false
     
     var body: some View {
-        NavigationView {
-            switch selectedTab {
-            case .history:
-                Text("History")
-                    .navigationTitle("History")
-                    .toolbarTitleDisplayMode(.inlineLarge)
-                
-            case .knowledge:
-                Text("Knowledge")
-                    .navigationTitle("Knowledge")
-                    .toolbarTitleDisplayMode(.inlineLarge)
-            }
-        }
-        .safeAreaInset(edge: .bottom) {
-            BottomTabBar(isCreateSheetOpen: $isCreateSheetOpen, selectedTab: $selectedTab, isShowingSearch: $isShowingSearch)
+        TabView {
+            
         }
         .sheet(isPresented: $isCreateSheetOpen) {
             Text("Create")
-        }
-        .sheet(isPresented: $isShowingSearch) {
-            Text("Search")
         }
     }
 }
